0fb3105526e579ecb4d41fd655289d40c75077c5
Shubham <shubhamlangade3@gmail.com>
2024-05-11 21:57:35 +0530

    Adding some operations

--- src/main/java/com/nt/REST/MsgRestController.java
+++ src/main/java/com/nt/REST/MsgRestController.java
@@ -1,47 +1,29 @@
 package com.nt.REST;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nt.binding.Customer;
+
 
 @RestController
 public class MsgRestController {
 
-	public MsgRestController() {
-		System.out.println("MsgRestController.MsgRestController()");
+	@GetMapping("/add")
+	public int gitAd(@RequestParam int p, @RequestParam int q) {
+	    return p+q;
+	}
+
+	@GetMapping("/mul")
+	public int gitMul(@RequestParam int p1, @RequestParam int p2) {
+	    return p1*p2;
 	}
 	
-	@GetMapping("/")
-	public ResponseEntity<String> getWelcomeMsg(){
-		
-		String msg="Welcome";
-		
-		return new ResponseEntity<>(msg,HttpStatus.OK);
-	}
 	
-	@GetMapping("/greet")
-	public String getGreetMsg()
-	{
-		String msg="Good morring";
-		
-		return msg;
-	}
-	
-	@GetMapping(value = "/obj", produces = {"application/xml", "application/json"})
-	public Customer custmor() {
-		
-		Customer c=new Customer();
-		
-		c.setEmail("Shubham3@gmail.com");
-		c.setName("Shubham");
-		c.setGender("Male");
-		
-		return c;
-	}
 	
 
 }
